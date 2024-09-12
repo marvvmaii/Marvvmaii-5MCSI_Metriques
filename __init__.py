fd10a7096e11c56f1be4296ba8bc313424b1813b
marvvmaii <75674722+marvvmaii@users.noreply.github.com>
2024-09-12 14:34:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,11 +5,11 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                    #commentaire                                                                                              
+app = Flask(__name__)                                                                                                                
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return render_template('hello.html') #commentaire
   
 if __name__ == "__main__":
   app.run(debug=True)
