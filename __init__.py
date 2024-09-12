b8c26388231f0206da6824b8bf2b2d8ee500a361
physiodose <101098273+physiodose@users.noreply.github.com>
2024-09-12 15:27:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('contact.html') #Comm2
+    return render_template('hello.html') #Comm2
 
 @app.route("/contact/")
 def MaPremiereAPI():
